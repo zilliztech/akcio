3d1515bc99ef5e1527e79fcee3ed0e2bce8bfcad
Mengjia Gu <mengjia.gu@zilliz.com>
2023-08-28 11:34:34 +0800

    Improve error handle
    Signed-off-by: Mengjia Gu <mengjia.gu@zilliz.com>

--- src_towhee/pipelines/insert/__init__.py
+++ src_towhee/pipelines/insert/__init__.py
@@ -9,12 +9,12 @@ def build_insert_pipeline(
         ):
     try:
         insert_pipeline = AutoPipes.pipeline(name, config=config)
-    except Exception:  # pylint: disable=W0703
+    except RuntimeError as e:  # pylint: disable=W0703
         if name.replace('-', '_') == 'generate_questions':
             sys.path.append(os.path.dirname(__file__))
             from generate_questions import custom_pipeline    # pylint: disable=c0415
 
             insert_pipeline = custom_pipeline(config=config)
         else:
-            raise AttributeError(f'Invalid insert mode: {name}')  # pylint: disable=W0707
+            raise RuntimeError(e) from e
     return insert_pipeline
